44cc7912b51b494905beba0d5b574fdd77f34bd9
Paresh Kacha <paresh.kacha.dev@gmail.com>
2021-05-13 12:14:31 +1200

    with latest code

--- Cyclistic Analysis Script.R
+++ Cyclistic Analysis Script.R
@@ -8,11 +8,11 @@ library(radiant)
 # easy date time operations
 library(lubridate)
 
-library(plyr)
 library(tidyverse)
+library(plyr)
 library(ggplot2)
 # Get all csv files from the directory
-current_path <- "/Users/pareshkacha/Documents/Capestone proj1/Cyclistic project data/"
+current_path <- "/Users/pareshkacha/Documents/Capestone proj1/Cyclistic_DA/Cyclistic project data/"
 setwd(current_path)
 
 files <- list.files(pattern = "*.csv", full.names=TRUE, recursive=FALSE)
@@ -41,57 +41,61 @@ for(i in 1:length(files)){ #
   total_records <- nrow(trip_data)
   
   # 2) Get records without missing value
-  records_without_missing_values <- trip_data[complete.cases(trip_data), ]
+  records_without_missing_ending_trip_data <- trip_data[complete.cases(trip_data), ]
   
   # 3) filter records further to check validation: positive time duration and time is not more then a day
-  ride_duration_in_secs <- as.numeric(difftime(strptime(records_without_missing_values$ended_at, "%Y-%m-%d %H:%M:%S", tz = "GMT"), strptime(records_without_missing_values$started_at, "%Y-%m-%d %H:%M:%S", tz = "GMT"), units = "secs"))
-  records_without_missing_values$ride_duration_in_secs <- round(ride_duration_in_secs, 2)
+  ride_duration_in_secs <- as.numeric(difftime(strptime(records_without_missing_ending_trip_data$ended_at, "%Y-%m-%d %H:%M:%S", tz = "GMT"), strptime(records_without_missing_ending_trip_data$started_at, "%Y-%m-%d %H:%M:%S", tz = "GMT"), units = "secs"))
+  records_without_missing_ending_trip_data$ride_duration_in_secs <- round(ride_duration_in_secs, 2)
   
-  records_with_valid_values <- filter(records_without_missing_values, ride_duration_in_secs > 0)
+  records_with_valid_ride_duration <- filter(records_without_missing_ending_trip_data, ride_duration_in_secs > 0)
   
   
   # 4.1) Get week day of start and end times for further analysis
-  records_with_valid_values$start_day <- weekdays(as.Date(records_with_valid_values$started_at))
-  records_with_valid_values$end_day <- weekdays(as.Date(records_with_valid_values$ended_at))
+  records_with_valid_ride_duration$start_day <- weekdays(as.Date(records_with_valid_ride_duration$started_at))
+  records_with_valid_ride_duration$end_day <- weekdays(as.Date(records_with_valid_ride_duration$ended_at))
   
   # 4.2) Get month of start and end times for further analysis
-  records_with_valid_values$start_ride_month <- months(as.Date(records_with_valid_values$started_at))
-  records_with_valid_values$end_ride_month <- months(as.Date(records_with_valid_values$ended_at))
+  records_with_valid_ride_duration$start_ride_month <- months(as.Date(records_with_valid_ride_duration$started_at))
+  records_with_valid_ride_duration$end_ride_month <- months(as.Date(records_with_valid_ride_duration$ended_at))
   
   # 4.3) Get hour and date of start times for further analysis
-  records_with_valid_values$start_ride_hour <- hour(records_with_valid_values$started_at)
-  records_with_valid_values$start_ride_date <- date(records_with_valid_values$started_at)
+  records_with_valid_ride_duration$start_ride_hour <- hour(records_with_valid_ride_duration$started_at)
+  records_with_valid_ride_duration$start_ride_date <- date(records_with_valid_ride_duration$started_at)
   
   # 5) Insert new row with distance in meter
-  records_with_valid_values$ride_distance_in_meter <- round(
+  records_with_valid_ride_duration$ride_distance_in_meter <- round(
     as_distance(
-      records_with_valid_values$start_lat, records_with_valid_values$start_lng, records_with_valid_values$end_lat, records_with_valid_values$end_lng, unit = "km"
+      records_with_valid_ride_duration$start_lat, records_with_valid_ride_duration$start_lng, records_with_valid_ride_duration$end_lat, records_with_valid_ride_duration$end_lng, unit = "km"
     ) * 1000
     , 2)
   
+  records_with_valid_ride_duration <- records_with_valid_ride_duration %>% filter(ride_distance_in_meter > 0)
+  
+  records_with_valid_distance <- nrow(records_with_valid_ride_duration)
   # 6) To check avg speed of ride in meter/second, to filter bias and consider riders who has actually ride the bike
-  records_with_valid_values$ride_avg_speed_in_meter_per_sec <- round(records_with_valid_values$ride_distance_in_meter/as.numeric(records_with_valid_values$ride_duration), 2)
+  records_with_valid_ride_duration$ride_avg_speed_in_meter_per_sec <- round(records_with_valid_ride_duration$ride_distance_in_meter/as.numeric(records_with_valid_ride_duration$ride_duration), 2)
   
-  records_with_valid_values <- records_with_valid_values %>% filter(ride_avg_speed_in_meter_per_sec > 0)
-  
-  records_with_valid_values <- records_with_valid_values %>% filter(ride_avg_speed_in_meter_per_sec < 20)
+  records_with_valid_ride_duration <- records_with_valid_ride_duration %>% filter(ride_avg_speed_in_meter_per_sec < 20)
   
   # Count total valid records
-  count_of_valid_records <- nrow(records_with_valid_values)
+  count_of_valid_records <- nrow(records_with_valid_ride_duration)
   
   # 7) check if 80% of data qualifies for the analysis
   ratio_of_valid_records <- (count_of_valid_records/total_records)*100
   print(ratio_of_valid_records)
   
-  # New file name using old one
-  newFileName <- paste(paste(finalFileName, "-validated", sep = ""), fileExtension, sep = ".") 
-  newPath <- paste(paste(current_path, "validated_data", sep = "/"), newFileName, sep = "/")
-  write.csv(records_with_valid_values, newPath)
-  
-  # str(records_with_valid_values)  
+  # data analysis for quality
+ data_quality_metrics <- c(total_records, nrow(records_without_missing_ending_trip_data), records_with_valid_distance, count_of_valid_records)
+ print(data_quality_metrics)
+ 
+ # New file name using old one
+ newFileName <- paste(paste(finalFileName, "-validated", sep = ""), fileExtension, sep = ".") 
+ newPath <- paste(paste(current_path, "validated_data", sep = "/"), newFileName, sep = "/")
+ write.csv(records_with_valid_ride_duration, newPath)
+ 
 }
 
-current_path <- "/Users/pareshkacha/Documents/Capestone proj1/Cyclistic project data/validated_data/"
+current_path <- "/Users/pareshkacha/Documents/Capestone proj1/Cyclistic_DA/Cyclistic project data/validated_data/"
 setwd(current_path)
 
 complete_trip_data <- ldply(list.files(), read.csv, header=TRUE)
@@ -103,8 +107,9 @@ ride_data <- complete_trip_data %>% group_by(start_day)
 temp_data <- complete_trip_data %>%
   group_by(start_ride_month, start_day, start_ride_date, start_ride_hour, rideable_type, member_casual) %>%
   dplyr::summarise(number_of_rides = n()/1000) 
+
 # start visualization from here%>%
-  ggplot(aes(x=start_ride_month, y=number_of_rides, color=member_casual)) +
+  ggplot(temp_data, aes(x=start_ride_month, y=number_of_rides, color=member_casual)) +
   geom_point() +
   geom_smooth() +
   # facet_wrap(~ member_casual) +
