59d2004dde02a5c143774b52d95faba2c2cbaf73
Paresh Kacha <paresh_kacha48@rocketmail.com>
2021-05-18 06:03:46 +1200

    With improved operations

--- Cyclistic Analysis Script.R
+++ Cyclistic Analysis Script.R
@@ -8,20 +8,38 @@ library(radiant)
 # easy date time operations
 library(lubridate)
 
+# Data set library
 library(tidyverse)
-library(plyr)
+
+# Data visualization library
 library(ggplot2)
+
 # Get all csv files from the directory
-current_path <- "/Users/pareshkacha/Documents/Capestone proj1/Cyclistic_DA/Cyclistic project data/"
+current_path <- "C:\\Users\\Paresh\\Documents\\Cyclistic\\Cyclistic_DA\\DataSets\\"
+
+# Set working directory for project
 setwd(current_path)
 
+# search for csv files, to automate the same operation for all the files
 files <- list.files(pattern = "*.csv", full.names=TRUE, recursive=FALSE)
-for(i in 1:length(files)){ # 
+
+# Fore loop of csv files
+for(i in 1:length(files)){
+  
+  # Use one file at a time at index "i"
   file <- files[i]
+  
+  # Split file name from its extension
   fileNameSplitName <- unlist(strsplit(file, "\\."))
+  
+  # Use file name for later usage, to store validated csv file
   finalFileName <- gsub("/", "", fileNameSplitName[2])
+  
   print(finalFileName)
+  
   fileExtension <- "csv"
+  
+  
   fileName <- paste(finalFileName, fileExtension, sep = ".")
   
   #  Read CSV files and store data in data frame
@@ -34,21 +52,19 @@ for(i in 1:length(files)){ #
     dplyr::mutate(duplicate_name = n()-1) %>%
     ungroup() %>%
     filter(duplicate_name > 0)
-  
   print(isDuplicate)
   
   # 1) total number of record in the data frame
   total_records <- nrow(trip_data)
   
   # 2) Get records without missing value
-  records_without_missing_ending_trip_data <- trip_data[complete.cases(trip_data), ]
-  
-  # 3) filter records further to check validation: positive time duration and time is not more then a day
+  records_without_missing_ending_trip_data <- trip_data[!apply(trip_data, 1, function(x) any(x=="")),] 
+
+  # 3) filter records further to check validation: positive time duration where time is not more then a day
   ride_duration_in_secs <- as.numeric(difftime(strptime(records_without_missing_ending_trip_data$ended_at, "%Y-%m-%d %H:%M:%S", tz = "GMT"), strptime(records_without_missing_ending_trip_data$started_at, "%Y-%m-%d %H:%M:%S", tz = "GMT"), units = "secs"))
   records_without_missing_ending_trip_data$ride_duration_in_secs <- round(ride_duration_in_secs, 2)
   
   records_with_valid_ride_duration <- filter(records_without_missing_ending_trip_data, ride_duration_in_secs > 0)
-  
   
   # 4.1) Get week day of start and end times for further analysis
   records_with_valid_ride_duration$start_day <- weekdays(as.Date(records_with_valid_ride_duration$started_at))
@@ -69,9 +85,11 @@ for(i in 1:length(files)){ #
     ) * 1000
     , 2)
   
+  # 5.1) filter rows that shows no ride distance
   records_with_valid_ride_duration <- records_with_valid_ride_duration %>% filter(ride_distance_in_meter > 0)
   
   records_with_valid_distance <- nrow(records_with_valid_ride_duration)
+  
   # 6) To check avg speed of ride in meter/second, to filter bias and consider riders who has actually ride the bike
   records_with_valid_ride_duration$ride_avg_speed_in_meter_per_sec <- round(records_with_valid_ride_duration$ride_distance_in_meter/as.numeric(records_with_valid_ride_duration$ride_duration), 2)
   
@@ -90,33 +108,15 @@ for(i in 1:length(files)){ #
  
  # New file name using old one
  newFileName <- paste(paste(finalFileName, "-validated", sep = ""), fileExtension, sep = ".") 
- newPath <- paste(paste(current_path, "validated_data", sep = "/"), newFileName, sep = "/")
+ newPath <- paste(paste(current_path, "validated_data", sep = ""), newFileName, sep = "\\")
  write.csv(records_with_valid_ride_duration, newPath)
- 
 }
 
-current_path <- "/Users/pareshkacha/Documents/Capestone proj1/Cyclistic_DA/Cyclistic project data/validated_data/"
-setwd(current_path)
-
-complete_trip_data <- ldply(list.files(), read.csv, header=TRUE)
-
-str(complete_trip_data)
-
-ride_data <- complete_trip_data %>% group_by(start_day)
-
-temp_data <- complete_trip_data %>%
-  group_by(start_ride_month, start_day, start_ride_date, start_ride_hour, rideable_type, member_casual) %>%
-  dplyr::summarise(number_of_rides = n()/1000) 
-
-# start visualization from here%>%
-  ggplot(temp_data, aes(x=start_ride_month, y=number_of_rides, color=member_casual)) +
-  geom_point() +
-  geom_smooth() +
-  # facet_wrap(~ member_casual) +
-  ggtitle("Number of rides for type of rideable and types of users in the last 12 months") +
-  ylab("Number of rides (1 000)") +
-  xlab("Date") +
-  theme(legend.position = "bottom") +
-  theme(legend.title = element_blank())
-
-View(ride_data)
+# current_path <- "C:\\Users\\Paresh\\Documents\\Cyclistic\\Cyclistic_DA\\DataSets\\validated_data\\"
+# setwd(current_path)
+# 
+# complete_trip_data <- ldply(list.files(), read.csv, header=TRUE)
+# 
+# complete_trip_data <- complete_trip_data %>% filter(nchar(complete_trip_data$start_station_name) == 0)
+# 
+# View(complete_trip_data)
